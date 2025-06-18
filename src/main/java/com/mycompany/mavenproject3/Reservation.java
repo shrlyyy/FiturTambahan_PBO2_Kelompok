f1aad867a16b2976b3c576cc0a6f8636f5be5f8f
Nopeli08 <nopeliba.nopeliba@itbss.ac.id>
2025-06-18 15:49:11 +0700

    Create Reservation.java

---
+++ src/main/java/com/mycompany/mavenproject3/Reservation.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject3;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Reservation {
+    private int reservationId;
+    private int customerId;
+    private LocalDate reservationDate;
+    private LocalTime reservationTime;
+    private String table;
+    private int numberOfPeople;
+    private String createdBy;
+    private String editedBy;
+    private String deletedBy;
+
+    public Reservation(int reservationId, int customerId, LocalDate reservationDate, LocalTime reservationTime, String table, int numberOfPeople, String createdBy, String editedBy, String deletedBy) {
+        this.reservationId = reservationId;
+        this.customerId = customerId;
+        this.reservationDate = reservationDate;
+        this.table = table;
+        this.numberOfPeople = numberOfPeople;
+        this.createdBy = createdBy;
+        this.editedBy = editedBy;
+        this.deletedBy = deletedBy;
+    }
+
+    public int getReservationId() { return reservationId; }
+    public void setReservationId(int reservationId) { this.reservationId = reservationId; }
+
+    public int getCustomerId() { return customerId; }
+    public void setCustomerId(int customerId) { this.customerId = customerId; }
+
+    public LocalDate getReservationDate() { return reservationDate; }
+    public void setReservationDate(LocalDate reservationDate) { this.reservationDate = reservationDate; }
+
+    public LocalTime getReservationTime() { return reservationTime; }
+    public void setReservationTime(LocalTime reservationTime) { this.reservationTime = reservationTime; }
+
+    public String getTable() { return table; }
+    public void setTable(String table) { this.table = table; }
+
+    public int getNumberOfPeople() { return numberOfPeople; }
+    public void setNumberOfPeople(int numberOfPeople) { this.numberOfPeople = numberOfPeople; }
+
+    public String getCreatedBy() { return createdBy; }
+    public void setCreatedBy(String createdBy) { this.createdBy = createdBy; }
+
+    public String getEditedBy() { return editedBy; }
+    public void setEditedBy(String editedBy) { this.editedBy = editedBy; }
+
+    public String getDeletedBy() { return deletedBy; }
+    public void setDeletedBy(String deletedBy) { this.deletedBy = deletedBy; }
+}
+
