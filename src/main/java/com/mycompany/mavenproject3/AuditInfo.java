ffc3050da1ae600a7dae90e48d8deabbd06dd702
Sherly <162469801+shrlyyy@users.noreply.github.com>
2025-06-25 06:38:23 +0700

    audit

---
+++ src/main/java/com/mycompany/mavenproject3/AuditInfo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class AuditInfo {
+    private String createdBy;
+    private String editedBy;
+    private String deletedBy;
+
+    public String getCreatedBy() { return createdBy; }
+    public void setCreatedBy(String createdBy) { this.createdBy = createdBy; }
+
+    public String getEditedBy() { return editedBy; }
+    public void setEditedBy(String editedBy) { this.editedBy = editedBy; }
+
+    public String getDeletedBy() { return deletedBy; }
+    public void setDeletedBy(String deletedBy) { this.deletedBy = deletedBy; }
+}
